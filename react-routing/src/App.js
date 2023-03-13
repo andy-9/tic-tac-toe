b6982a761a754c7fc894ee2e9d97466fb1bd5850
Andreas Hechler <info@andreashechler.com>
2023-03-13 23:28:54 +0100

    adjusting code for react-router-dom v6.9

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./App.css";
 import Nav from "./Nav";
+import Home from "./Home";
 import About from "./About";
 import Shop from "./Shop";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<Nav />
-			<About />
-			<Shop />
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/shop" element={<Shop />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
